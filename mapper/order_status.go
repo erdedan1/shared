c60118d7eb8d59344c3d007ef2f8e18ca0ee58bf
erdedani <51397189+RadnaevErdeni@users.noreply.github.com>
2026-02-18 21:10:54 +0800

    orderstatus

--- mapper/order_status.go
+++ mapper/order_status.go
@@ -1,23 +1,14 @@
 package proto_mapper
 
 import (
+	"errors"
+
 	pb "github.com/erdedan1/protocol/proto/order_service/gen"
 )
 
-func ProtoOrderStatusToString(protoOrderStatus []pb.OrderStatus) []string {
-	orderStatus := make([]string, 0, len(protoOrderStatus))
-	for _, status := range protoOrderStatus {
-		orderStatus = append(orderStatus, status.String())
+func StringOrderStatusToProto(strOrderStatus string) (*pb.OrderStatus, error) {
+	if r, ok := pb.OrderStatus_value[strOrderStatus]; ok {
+		return pb.OrderStatus(r).Enum(), nil
 	}
-	return orderStatus
-}
-
-func StringOrderStatusToProto(strOrderStatus []string) []pb.OrderStatus {
-	orderStatus := make([]pb.OrderStatus, 0, len(strOrderStatus))
-	for _, status := range strOrderStatus {
-		if r, ok := pb.OrderStatus_value[status]; ok {
-			orderStatus = append(orderStatus, pb.OrderStatus(r))
-		}
-	}
-	return orderStatus
+	return nil, errors.New("")
 }
