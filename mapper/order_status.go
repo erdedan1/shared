7d5af72242ef9ebf32b25f682b70fb132b11518e
erdedani <51397189+RadnaevErdeni@users.noreply.github.com>
2026-02-18 21:19:58 +0800

    delete pointer

--- mapper/order_status.go
+++ mapper/order_status.go
@@ -4,9 +4,9 @@ import (
 	pb "github.com/erdedan1/protocol/proto/order_service/gen"
 )
 
-func StringOrderStatusToProto(strOrderStatus string) *pb.OrderStatus {
+func StringOrderStatusToProto(strOrderStatus string) pb.OrderStatus {
 	if r, ok := pb.OrderStatus_value[strOrderStatus]; ok {
-		return pb.OrderStatus(r).Enum()
+		return pb.OrderStatus(r)
 	}
-	return pb.OrderStatus_ORDER_STATUS_UNSPECIFIED.Enum()
+	return pb.OrderStatus_ORDER_STATUS_UNSPECIFIED
 }
