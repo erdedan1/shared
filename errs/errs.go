57b1abaf5a4a3ddf24c96b9b8017d4137cadfd93
erdedani <erdeni1313@gmail.com>
2026-03-23 14:59:48 +0800

    errs

--- errs/errs.go
+++ errs/errs.go
@@ -11,19 +11,31 @@ type CustomError struct {
 }
 
 func (c *CustomError) Error() string {
+	if c.Err != nil {
+		return fmt.Sprintf("code: %d, message: %s, err: %v", c.Code, c.Message, c.Err)
+	}
 	return fmt.Sprintf("code: %d, message: %s", c.Code, c.Message)
 }
 
-func New(code ErrorCode, msg string, err error) *CustomError {
+func New(code ErrorCode, msg string, err ...error) *CustomError {
+	var e error
+	if len(err) > 0 {
+		e = err[0]
+	}
+
 	return &CustomError{
 		Code:    code,
 		Message: msg,
-		Err:     err,
+		Err:     e,
 	}
 }
 
 func (c *CustomError) Unwrap() error {
 	return c.Err
+}
+
+func (c *CustomError) Is(target error) bool {
+	return c == target
 }
 
 type ErrorCode int
